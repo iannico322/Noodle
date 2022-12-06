7e29b995f62720963d791ef4c70861eca624e890
iannico322 <73187566+iannico322@users.noreply.github.com>
2022-12-06 19:15:12 +0800

    adding lyrics

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,6 +1,6 @@
-$(window).on('load',function(){
+$(window).on('load', function () {
     $(".loading").fadeOut(2000);
-    
+
     $(".container").fadein(1000);
 })
 
@@ -15,163 +15,204 @@ const dancing = document.querySelector('.dancing')
 const BlueSky = document.querySelector('.BlueSky')
 const operationHappy = document.querySelector('.Lucky')
 
-const instru1= document.querySelector('.instru1')
+const instru1 = document.querySelector('.instru1')
 const instru2 = document.querySelector('.instru2')
 
 const ring = document.querySelector('.ring')
 
 const h1 = document.querySelector('.h1').classList
-function GoogleAPI(){
-    const query = `https://www.google.com/search?q=${search_query.value}`
-    console.log(query)
-    if (search_query.value == ""){
 
-    }else{
+
+
+const MusicLyrics = [
+    "Mr. Blue Sky by Electric Light Orchestra",
+    "Sun is shinin' in the sky",
+    "There ain't a cloud in sight",
+    "It's stopped rainin' everybody's in the play",
+    "And don't you know",
+    "It's a beautiful new day, hey hey",
+    "Runnin' down the avenue",
+    "See how the sun shines brightly in the city",
+    "On the streets where once was pity",
+    "Mr. Blue Sky is living here today, hey hey",
+    "Mr. Blue Sky please tell us why",
+    "You had to hide away for so long (so long)",
+    "Where did we go wrong?",
+    "Mr. Blue Sky please tell us why",
+    "You had to hide away for so long (so long)",
+    "Where did we go wrong?",
+    "Mr Blue Sky - instrumental",
+    ""
+
+]
+const verseIndex = [2,10, 13,16,20,22,26,30,34,36,44,46,51,54,57,62,67,82]
+
+let interval = 0;
+BlueSky.addEventListener("timeupdate", () => {
+    if (Math.floor(BlueSky.currentTime) > verseIndex[interval]) {
+        search_query.value = MusicLyrics[interval];
+        interval++;
+    }
+    console.log(Math.floor(BlueSky.currentTime)  )
+    console.log(Math.floor(verseIndex[interval])  )
+  });
+
+
+
+function GoogleAPI() {
+    const query = `https://www.google.com/search?q=${search_query.value}`
+
+    if (search_query.value == "") {
+
+    } else {
         searching.href = query
         searching.click()
     }
 }
 
-searchIcon.onclick = ()=>{
+searchIcon.onclick = () => {
     GoogleAPI()
 }
 
-searchBtn.onclick = ()=>{
+searchBtn.onclick = () => {
     GoogleAPI()
-    
+
 }
 
 
-let x = 2
-operationHappy.onclick= ()=>{
-    
-    if(x %2 == 0){
+let x = true
+operationHappy.onclick = () => {
+
+    if (x) {
+
         bg.style.opacity = "100"
         BlueSky.play()
         h1.add('active')
         operationHappy.style.color = "#f8f9fa"
         operationHappy.style.backgroundColor = "#ea4335"
-        x++
-    }else{
+        x = false
+    } else {
         bg.style.opacity = "0"
         BlueSky.pause()
         operationHappy.style.color = "black"
         operationHappy.style.backgroundColor = "#f8f9fa"
         h1.remove('active')
-        x++
+        x = true
     }
-    
-    
+
+
 }
 
-search_query.addEventListener('mouseover',()=>{
+search_query.addEventListener('mouseover', () => {
     h1.add('active')
 })
-search_query.addEventListener('mouseout',()=>{
+search_query.addEventListener('mouseout', () => {
     h1.remove('active')
 })
 
-BlueSky.addEventListener('timeupdate',()=>{
+BlueSky.addEventListener('timeupdate', () => {
 
-        seconds = BlueSky.currentTime
-        minutes = Math.floor( seconds/ 60);
-        minutes = (minutes >= 10) ? minutes : "0" + minutes;
-        seconds = Math.floor(seconds % 60);
-        seconds = (seconds >= 10) ? seconds : "0" + seconds;
-        time = minutes + ":" + seconds
-        console.log( time )
-        if(time == "00:00"){
-            ring.src = "./resources/media/img/luckyME2.gif"
-            instru1.src = ""
-            dancing.src = "./resources/media/img/dance.gif"
-            instru2.src = "./resources/media/img/guitar.gif"
-        }
-        if ( time == "00:04"){
-            cover.style.opacity = "1"
-            instru2.src = "./resources/media/img/luckyME3.gif"
-            dancing.src = "./resources/media/img/luckyME4.gif"
-        }else if( time == "00:05"){
-            cover.style.opacity = "0"
-            instru1.src = "./resources/media/img/luckyME.gif"
-            dancing.src = "./resources/media/img/dance2.gif"
-        }else if(time =="00:07" ){
-            
-            dancing.src = "./resources/media/img/dance3.gif"
-        }else if(time =="00:11" ){
-            instru1.style.transform = "scaleX(1)"
-            instru1.src = "./resources/media/img/sing.gif"
-            
-        }else if (time =="00:14" ){
-            instru1.style.transform = "scaleX(-1)"
-            instru1.src = "./resources/media/img/sing.gif"
-            
-        }
-        else if (time =="00:19" ){
-            instru1.style.transform = "scaleX(1)"
-            instru1.src = "./resources/media/img/sing.gif"
-            
-        }
+    seconds = BlueSky.currentTime
+    minutes = Math.floor(seconds / 60);
+    minutes = (minutes >= 10) ? minutes : "0" + minutes;
+    seconds = Math.floor(seconds % 60);
+    seconds = (seconds >= 10) ? seconds : "0" + seconds;
+    time = minutes + ":" + seconds
+
+    if (time == "00:00") {
+        ring.src = "./resources/media/img/luckyME2.gif"
+        instru1.src = ""
+        dancing.src = "./resources/media/img/dance.gif"
+        instru2.src = "./resources/media/img/guitar.gif"
+    }
+    if (time == "00:04") {
+        cover.style.opacity = "1"
+        instru2.src = "./resources/media/img/luckyME3.gif"
+        dancing.src = "./resources/media/img/luckyME4.gif"
+    } else if (time == "00:05") {
+        cover.style.opacity = "0"
+        instru1.src = "./resources/media/img/luckyME.gif"
+        instru2.src = ""
+        dancing.src = "./resources/media/img/dance2.gif"
+    } else if (time == "00:07") {
+
+        dancing.src = "./resources/media/img/dance3.gif"
+    } else if (time == "00:11") {
+        instru1.style.transform = "scaleX(1)"
+        instru2.src = "./resources/media/img/luckyME3.gif"
+        instru1.src = "./resources/media/img/sing.gif"
+
+    } else if (time == "00:14") {
+        instru1.style.transform = "scaleX(-1)"
+        instru1.src = "./resources/media/img/sing.gif"
+
+    }
+    else if (time == "00:19") {
+        instru1.style.transform = "scaleX(1)"
+        instru1.src = "./resources/media/img/sing.gif"
+
+    }
 
 
-        if ( time == "00:26"){
-            cover.style.opacity = "1"
-            instru1.style.transform = "scaleX(-1)"
-            dancing.src = "./resources/media/img/luckyME.gif"
-            ring.src = "./resources/media/img/ring1.gif"
-        }else if( time == "00:27"){
-            instru1.style.transform = "scaleX(1)"
-            cover.style.opacity = "0"
+    if (time == "00:26") {
+        cover.style.opacity = "1"
+        instru1.style.transform = "scaleX(-1)"
+        dancing.src = "./resources/media/img/luckyME.gif"
+        ring.src = "./resources/media/img/ring1.gif"
+    } else if (time == "00:27") {
+        instru1.style.transform = "scaleX(1)"
+        cover.style.opacity = "0"
 
-        }
+    }
 
-        if( time == "00:28"){
-            instru1.style.transform = "scaleX(1)"
-    
+    if (time == "00:28") {
+        instru1.style.transform = "scaleX(1)"
+    }
 
-        }
+    //45   54  109
+    if (time == "00:42") {
+        cover.style.opacity = "1"
+        dancing.src = "./resources/media/img/luckyME.gif"
+        ring.src = "./resources/media/img/ring1.gif"
+    } else if (time == "00:43") {
+        // 43 49 55 101
+        cover.style.opacity = "0"
+        instru1.src = "./resources/media/img/sing3.gif"
+        instru2.src = ""
+        dancing.src = "./resources/media/img/dance4.gif"
+    } else if (time == "00:49") {
+        instru2.src = ""
+        instru1.src = "./resources/media/img/sing2.gif"
+    } else if (time == "00:55") {
+        instru1.src = "./resources/media/img/sing3.gif"
+    } else if (time == "01:00") {
+        dancing.src = "./resources/media/img/sleep.gif"
+        instru1.src = "./resources/media/img/sing2.gif"
+    }
+    else if (time == "01:08") {
+        cover.style.opacity = "1"
+        dancing.src = "./resources/media/img/luckyMe4.gif"
+        instru2.src = "./resources/media/img/sing4.gif"
+        instru1.src = "./resources/media/img/luckyME.gif"
+        ring.src = "./resources/media/img/dance3.gif"
+    } else if (time == "01:16") {
+        cover.style.opacity = "1"
+        dancing.src = "./resources/media/img/dance3.gif"
+        instru2.src = "./resources/media/img/sing4.gif"
+        instru1.src = "./resources/media/img/luckyME.gif"
+        ring.src = "./resources/media/img/guitar.gif"
+    }
 
-        //45   54  109
-        if ( time == "00:42"){
-            cover.style.opacity = "1"
-            dancing.src = "./resources/media/img/luckyME.gif"
-            ring.src = "./resources/media/img/ring1.gif"
-        }else if( time == "00:43"){
-            // 43 49 55 101
-            cover.style.opacity = "0"
-            instru1.src = "./resources/media/img/sing3.gif"
-            instru2.src = ""
-             dancing.src = "./resources/media/img/dance4.gif"
-        }else if( time == "00:49"){
-            instru2.src = ""
-            instru1.src = "./resources/media/img/sing2.gif"
-        }else if( time == "00:55"){
-            instru1.src = "./resources/media/img/sing3.gif"
-        }else if(time == "01:00"){
-            dancing.src = "./resources/media/img/sleep.gif"
-            instru1.src = "./resources/media/img/sing2.gif"
-        }
-        else if(time == "01:08"){
-            cover.style.opacity = "1"
-            dancing.src = "./resources/media/img/luckyMe4.gif"
-            instru2.src = "./resources/media/img/sing4.gif"
-            instru1.src = "./resources/media/img/luckyME.gif"
-            ring.src = "./resources/media/img/dance3.gif"
-        }else if(time == "01:16"){
-            cover.style.opacity = "1"
-            dancing.src = "./resources/media/img/dance3.gif"
-            instru2.src = "./resources/media/img/sing4.gif"
-            instru1.src = "./resources/media/img/luckyME.gif"
-            ring.src = "./resources/media/img/guitar.gif"
-        }
+    BlueSky.addEventListener("ended", () => {
+        interval = 0;
+        x = true
+        bg.style.opacity = "0"
+        h1.remove('active')
+        operationHappy.style.color = "black"
+        operationHappy.style.backgroundColor = "#f8f9fa"
+        cover.style.opacity = "0"
+    })
 
-        BlueSky.addEventListener("ended",()=>{
-            bg.style.opacity = "0"
-            h1.remove('active')
-            operationHappy.style.color = "black"
-            operationHappy.style.backgroundColor = "#f8f9fa"
-            cover.style.opacity = "0"
-          })
-    
 
-        
+
 })
